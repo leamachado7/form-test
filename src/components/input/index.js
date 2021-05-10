71a170b94a9b10f36b0afe750f255683af770425
Leandro <lmachado@shaman.uy>
2021-05-10 17:48:11 -0300

    code is more organized, value is handled externally

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import './index.scss'
-export const Input = ({ label, required, disabled, minLength }) => {
+export const Input = ({ field,onChange, label, required, disabled, minLength , value}) => {
+  const onChanged = (e) => {
+    onChange(field, e.target.value)
+  }
   return (
     <div className="cmp-input-container">
       <label className='cmp-input-label'>{label} {required ? '*' : ''}</label>
-      <input minLength={minLength} disabled={disabled} className='cmp-input' type='text' />
+      <input value={value} onChange={onChanged} minLength={minLength} disabled={disabled} className='cmp-input' type='text' />
     </div>
   )
 }
--- src/containers/signup/index.js
+++ src/containers/signup/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
 import { FORM_KEYS, FORM_LABELS, SELECT_OPTIONS, INPUT_TYPES } from '../../global/constants';
 
@@ -6,17 +6,25 @@ import Input from '../../components/input';
 import CustomSelect from '../../components/select';
 import Checkbox from '../../components/checkbox';
 export const Signup = () => {
-  const [form, setForm] = useState({
-
-  })
-  const onChangeCheckbox = (field, value) => {
-    const newForm = {...form}
-    newForm[field] = value;
-    setForm(newForm)
-  }
-  useEffect(() => {
-    console.log(form)
-  }, [form])
+	const [form, setForm] = useState({
+		firstName: '',
+		lastNAme: '',
+		email: '',
+		advances: false,
+		alerts: false,
+		comms: false
+	});
+	const onChange = (field, value) => {
+		const newForm = { ...form };
+		newForm[field] = value;
+		setForm(newForm);
+	};
+	useEffect(
+		() => {
+			console.log(form);
+		},
+		[form]
+	);
 	return (
 		<div className="page">
 			<div className="container">
@@ -26,18 +34,32 @@ export const Signup = () => {
 				</div>
 				<div className="form-layout">
 					<div className="input-container">
-						<Input type={INPUT_TYPES.TEXT} label={FORM_LABELS.FIRST_NAME} disabled required minLength="3" />
-						<Input type={INPUT_TYPES.TEXT} label={FORM_LABELS.LAST_NAME} required minLength="3" />
-						<Input type={INPUT_TYPES.EMAIL} label={FORM_LABELS.EMAIL} required minLength="3" />
-						<Input type={INPUT_TYPES.TEXT} label={FORM_LABELS.ORGANIZATION} />
+						<Input field='firstName' value={form.firstName} onChange={onChange} type={INPUT_TYPES.TEXT} label={FORM_LABELS.FIRST_NAME} required minLength="3" />
+						<Input field='lastName' value={form.lastName} onChange={onChange} type={INPUT_TYPES.TEXT} label={FORM_LABELS.LAST_NAME} required minLength="3" />
+						<Input field='email' value={form.email} onChange={onChange} type={INPUT_TYPES.EMAIL} label={FORM_LABELS.EMAIL} required minLength="3" />
+						<Input field='organization' value={form.organization} onChange={onChange} type={INPUT_TYPES.TEXT} label={FORM_LABELS.ORGANIZATION} />
 					</div>
 					<div className="select-container">
 						<CustomSelect label={FORM_LABELS.EU_RESIDENT} options={SELECT_OPTIONS} required />
 					</div>
 					<div className="checkbox-wrapper">
-						<Checkbox onChange={onChangeCheckbox} value={form.advances} field='advances' label={FORM_KEYS.ADVANCES} />
-						<Checkbox onChange={onChangeCheckbox} value={form.alerts} field='alerts' label={FORM_KEYS.ALERTS} />
-						<Checkbox onChange={onChangeCheckbox} value={form.comms} field='comms' />
+						<Checkbox
+							field="advances"
+							onChange={onChange}
+							value={form.advances}
+							label={FORM_KEYS.ADVANCES}
+						/>
+						<Checkbox
+							field="alerts"
+							onChange={onChange}
+							value={form.alerts}
+							label={FORM_KEYS.ALERTS}
+						/>
+						<Checkbox
+							field="comms"
+							onChange={onChange}
+							value={form.comms}
+							label={FORM_KEYS.COMMS} />
 					</div>
 					<div className="btn-container">
 						<button className="btn-submit">SUBMIT</button>
